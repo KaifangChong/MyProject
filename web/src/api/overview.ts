6cacbe82e0fa10351513a486560fac538f8f61c9
KaifangChong <kc3359@nau.edu>
2024-10-22 17:55:27 +0800

    index.html调整

--- web/src/api/overview.ts
+++ web/src/api/overview.ts
@@ -1,16 +1,14 @@
-import { get } from '/@/utils/http/axios';
+import {get} from '/@/utils/http/axios';
 
 enum URL {
     list = '/api/overview/count',
-    sysInfo = '/api/overview/sysInfo',
 }
 
 // 简化API方法定义
 const apiGet = async (url: string, params: any = {}) =>
-    get<any>({ url, params });
+    get<any>({url, params});
 
 // API方法
 const listApi = (params: any) => apiGet(URL.list, params);
-const sysInfoApi = (params: any) => apiGet(URL.sysInfo, params);
 
-export { listApi, sysInfoApi };
+export {listApi};
