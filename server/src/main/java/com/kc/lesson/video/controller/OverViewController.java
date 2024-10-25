dbb11b1ac6b86e670b5bfc4900ebc11834883f09
KaifangChong <kc3359@nau.edu>
2024-10-25 19:47:58 +0800

    调整注释

--- server/src/main/java/com/kc/lesson/video/controller/OverViewController.java
+++ server/src/main/java/com/kc/lesson/video/controller/OverViewController.java
@@ -60,7 +60,7 @@ public class OverViewController {
         visitData.put("day", day);
 
         List<VisitData> webVisitData = overviewMapper.getWebVisitData(day);
-        int pv = webVisitData.stream().mapToInt(visit -> visit.count).sum(); // 计算播放数量
+        int pv = webVisitData.stream().mapToInt(visit -> visit.count).sum(); // 计算访问数量
         int uv = webVisitData.size(); // 计算IP的数量
 
         visitData.put("pv", String.valueOf(pv));
